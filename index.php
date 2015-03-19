485c2e7fac5b7c61fa3520eb0d6e5f3cc9cc713c
Piotr Halicki <Piotr_Halicki@wp.pl>
2015-03-19 22:59:35 +0100

    Wartości przekazane

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>						<!-- deklaracja języka HTML -->
+
+<html lang="pl-PL">					<!-- deklaracja [języka strony]-[KRAJU] -->
+	<head>							<!-- sekcja nagłówkowa -->
+		<meta charset="UTF-8">		<!-- zestaw znaków  / [nazwa znacznika]=["wartość"]-->
+		<meta name="Przekazywanie informacji" content="przekazywanie informacji, hiperłącze">	
+		<title>						
+		Przekazywanie informacji między stronami - 1 - Hiperłącze
+		</title>					<!-- tytuł strony (niewidoczny) -->
+	</head>
+
+	<body>							<!-- "ciało" strony -->
+
+	<h1> 							<!-- "heading", nagłówek -->
+	<strong><em>Przekazywanie informacji między stronami</em></strong>
+	</h1>
+		<br>
+	<hr>
+		<br>
+	<h2> 							<!-- nagłówek "mniej" znaczący ;) -->
+	<strong>1 Hiperłącze</strong>
+	</h2>
+
+	<a href="odbiorca.php?name=piotr&surname=halicki&city=warszawa">Click me</a>
+	
+	</body>
+
+</html>								<!-- znacznik zamknięcia -->
---
+++ odbiorca.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>						<!-- deklaracja języka HTML -->
+
+<html lang="pl-PL">					<!-- deklaracja [języka strony]-[KRAJU] -->
+	<head>							<!-- sekcja nagłówkowa -->
+		<meta charset="UTF-8">		<!-- zestaw znaków  / [nazwa znacznika]=["wartość"]-->
+		<meta name="Przekazywanie informacji" content="przekazywanie informacji, hiperłącze">	
+		<title>						
+		Przekazywanie informacji między stronami - 1 - Hiperłącze
+		</title>					<!-- tytuł strony (niewidoczny) -->
+	</head>
+
+	<body>							<!-- "ciało" strony -->
+
+	<h1> 							<!-- "heading", nagłówek -->
+	<strong><em>Przekazywanie informacji między stronami</em></strong>
+	</h1>
+		<br>
+	<hr>
+		<br>
+	<h2> 							<!-- nagłówek "mniej" znaczący ;) -->
+	<strong>1 Hiperłącze</strong>
+	</h2>
+
+<?php
+	echo 'Parametr 1 ', '<strong><u>', '(imię)', '</u></strong>', ' ma wartość ', '<strong>' .ucfirst($_GET['name']), '</strong>';
+	echo '<br>';
+	echo 'Parametr 2 ', '<strong><u>', '(nazwisko)', '</u></strong>', ' ma wartość ', '<strong>' .ucfirst($_GET['surname']), '</strong>';
+	echo '<br>';
+	echo 'Parametr 3 ', '<strong><u>', '(miasto)', '</u></strong>', ' ma wartość ', '<strong>' .ucfirst($_GET['city']), '</strong>';
+?>
+	
+	</body>
+
+</html>								<!-- znacznik zamknięcia -->
